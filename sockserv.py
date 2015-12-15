7085ade11167b3999a7b59de17735fa9640e49b3
JoakinKirschen <joakin.kirschen@gmail.com>
2015-12-15 00:31:09 +0000

    Commit message

--- sockserv.py
+++ sockserv.py
@@ -28,52 +28,29 @@ class database(object):
             self.setupDefaultData()
     
     def setupDefaultData(self):
-       """ create the database tables and default values if it doesn't exist already """
-       cursor = db.cursor()
-       CREATE TABLE IF NOT EXISTS movement
-       (
+        """ create the database tables and default values if it doesn't exist already """
+        cursor = db.cursor()
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS movement
+        (
             id         INTEGER     PRIMARY KEY     AUTOINCREMENT,
             name    TEXT,
             value      TEXT
-       ) ;
-
-
-
-    # Get a cursor object
-    cursor = db.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, name TEXT,
-                           phone TEXT, email TEXT unique, password TEXT)
-    ''')
-    db.commit()
-    cursor.close()
-
-
-    cursor = db.cursor()
-    name1 = 'Andres'
-    phone1 = '3366858'
-    email1 = 'user@example.com'
-    # A very secure password
-    password1 = '12345'
-     
-    name2 = 'John'
-    phone2 = '5557241'
-    email2 = 'johndoe@example.com'
-    password2 = 'abcdef'
-     
-    # Insert user 1
-    cursor.execute('''INSERT INTO users(name, phone, email, password)
+        )
+        ''')
+        db.commit()
+        cursor.close()
+    
+    def newMovementQuery(self,movementname):
+        cursor = db.cursor()
+        # Insert user 1
+        cursor.execute('''INSERT INTO users(name, phone, email, password)
                       VALUES(?,?,?,?)''', (name1,phone1, email1, password1))
-    print('First user inserted')
-     
-    # Insert user 2
-    cursor.execute('''INSERT INTO users(name, phone, email, password)
-                      VALUES(?,?,?,?)''', (name2,phone2, email2, password2))
-    print('Second user inserted')
-     
-    db.commit()
+        print('New movement created')
+        db.commit()
 
-    db.close()
+    def closedb(self,movementname):
+        db.close()
 # ===========================================================================
 # Example Code
 # ===========================================================================
