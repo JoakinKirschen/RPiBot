bdd235ec4ec00b152c5c99ee84106017ca51a627
joakin.kirschen@gmail.com <joakin.kirschen@gmail.com>
2016-09-04 20:24:20 +0000

    Commit message

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 # upload to github
 git add *
 git commit -m "Commit message"
-git remote add origin https://github.com/JoakinKirschen/RPiBot.git
+# git remote add origin https://github.com/JoakinKirschen/RPiBot.git
 # git remote add origin https://github.com/JoakinKirschen/RPiBot.git
 git push -u origin master
